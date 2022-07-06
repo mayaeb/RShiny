84737573a713f16b6b8a8765e9284adc50591463
Maya Emmons-Bell <maya.emmonsbell@gmail.com>
2022-07-06 12:33:06 -0400

    Adding features - MA plot, count plot clickability

--- finregen_RNAseq/app.R
+++ finregen_RNAseq/app.R
@@ -35,23 +35,46 @@ dds <- DESeqDataSetFromMatrix(countData=matrix,
 #specify the order of dpa factor
 dds$dpa <- factor(dds$dpa, levels = c("uninjured","4DPA"))
 dds <- DESeq(dds)
+results <- results(dds)
 
+#specify size of plots
+size = 450
+#specify max y value for MA plot 
+ymax <- 5
 
 # Define UI for application 
 ui <- fluidPage(
     
     # Application title
-    titlePanel("Regenerating Caudal Fin Tissue: Uninjured vs. 4DPA"),
+    titlePanel("Regenerating caudal fin tissue: bulk RNAseq"),
     
-    #User selects gene name from external gene name list 
-    selectizeInput("gene", label = "Enter gene symbols of interest:", choices = NULL, multiple = FALSE),
+    sidebarLayout(
+      sidebarPanel(
+        #User selects gene name from external gene name list 
+        selectizeInput("gene", label = "Enter gene symbols of interest:", choices = NULL, multiple = FALSE),
+        
+      ),
     
+    mainPanel(
     #output controls that tell Shiny where to put rendered output (text and plot)
+    #desriptive text above stat table
     textOutput("text"),
+    #table of stats from DEseq2
     tableOutput("static"),
-    plotOutput("plot")
-    
+    #plot of normalized counts for selected gene 
+    plotOutput("counts_plot", width = size, height = size, click = "counts_plot_click"),
+    #sample-level count information (based on clicked point)
+    textOutput("counts_info"),
+    #text title for MA plot
+    verbatimTextOutput("ma_plot_title"),
+    #MA plot - with adjustable slider and clickable points 
+    plotOutput("ma_plot", width = size, height = size),
+    #MA plot slider 
+    sliderInput("alpha", "Adjusted p-value threshold", min = 0, max = 0.5, value = 0.1, step = 0.001, width = size)
     )
+    
+  )
+)
 
 # Define server logic for generating summary stats and plot
 server <- function(input, output, session) {
@@ -74,9 +97,29 @@ server <- function(input, output, session) {
         data[data$`Gene Name`%in%gene(), 1:7]
     })
 
-    output$plot <- renderPlot({
+    output$counts_plot <- renderPlot({
         ensembl_id <- c(as.character(data[data$`Gene Name`%in%gene(), 1]))
-        plotCounts(dds, gene = ensembl_id, intgroup = "dpa", xlab = "Timepoint")
+        p <- plotCounts(dds, gene = ensembl_id, intgroup = "dpa", returnData = TRUE)
+        ggplot(p, aes(x=dpa, y=count)) +
+          geom_point(position = position_jitter(w=0.1, h=0)) +
+          scale_y_log10(breaks=c(25, 100, 400)) +
+          xlab("Timepoint") +
+          theme_classic(base_size = 20)
+    })
+    
+    output$counts_info <- renderText({
+      counts_str <- function(e) {
+        if(is.null(e)) return("NULL \n")
+        paste0("Normalized counts: ", round(e$y, 1), "\n")
+        
+      }
+
+    paste0(counts_str(input$counts_plot_click)) 
+    })
+
+    output$ma_plot <- renderPlot({
+      par( mar = c(5, 5, 3, 2), cex.main = 1.5, cex.lab = 1.35)
+      plotMA(results, ylim = c(-ymax, ymax), alpha = input$alpha)
     })
 
 }
